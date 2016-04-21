577801ac21c0bfa79d59c6d2318cf8658436cd6c
Philippe Vaucher <philippe@stvs.ch>
2016-04-21 16:40:01 +0200

    Add commentary

--- docker.el
+++ docker.el
@@ -25,6 +25,32 @@
 
 ;;; Commentary:
 
+;; # Emacs interface to Docker!
+;;
+;; This package allows you to manipulate docker images, containers & more from Emacs.
+;;
+;; # API
+;;
+;; | command                     | description                                                     |
+;; |-----------------------------|-----------------------------------------------------------------|
+;; | docker-images               | list images                                                     |
+;; | docker-rmi                  | delete image                                                    |
+;; | docker-pull                 | pull image                                                      |
+;; | docker-push                 | push image                                                      |
+;; | docker-run                  | run image                                                       |
+;; | docker-containers           | list containers                                                 |
+;; | docker-rm                   | delete container                                                |
+;; | docker-stop                 | stop container                                                  |
+;; | docker-pause                | pause container                                                 |
+;; | docker-restart              | restart container                                               |
+;; | docker-start                | start container                                                 |
+;; | docker-unpause              | unpause container                                               |
+;; | docker-volumes              | list volumes                                                    |
+;; | docker-volume-rm            | delete volume                                                   |
+;; | docker-networks             | list networks                                                   |
+;; | docker-network-rm           | delete network                                                  |
+;; | dockerfile-build-buffer     | Build [Dockerfile](https://github.com/spotify/dockerfile-mode)  |
+
 ;;; Code:
 
 (require 'docker-images)
