3ff8ea78c2b39884e3fe571947c3d35dc8a8fce6
Josh Bax <joshbax189@gmail.com>
2021-10-07 15:01:35 +1300

    Prettify docker transient

--- docker.el
+++ docker.el
@@ -57,10 +57,10 @@
    (5 "Tk" "TLS key" "--tlskey" docker-read-certificate)
    (5 "l" "Log level" "--log-level " docker-read-log-level)]
   ["Docker"
-   ("c" "Containers" docker-containers)
-   ("i" "Images"     docker-images)
-   ("n" "Networks"   docker-networks)
-   ("v" "Volumes"    docker-volumes)]
+   ("c" docker-container-description-with-stats docker-containers)
+   ("i" docker-image-description-with-stats     docker-images)
+   ("n" docker-network-description-with-stats   docker-networks)
+   ("v" docker-volume-description-with-stats    docker-volumes)]
   ["Other"
    ("C" "Compose"    docker-compose)
    ("M" "Machines"   docker-machines)])
